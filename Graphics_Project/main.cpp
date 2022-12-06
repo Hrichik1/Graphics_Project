d65d2171e7457195fbecb43ab41520eb9d108f66
Hrichik Paul Ankan <88372396+Hrichik1@users.noreply.github.com>
2022-12-06 21:54:09 +0600

    Day-Sun-River

--- Graphics_Project/main.cpp
+++ Graphics_Project/main.cpp
@@ -4,7 +4,8 @@
 #include<math.h>>
 # define PI           3.14159265358979323846
 
-//----dayRiver----
+ //----Day---------//
+
 void dayRiver(){
 	//GL_QUADS
     glBegin(GL_QUADS);
@@ -19,49 +20,101 @@ void dayRiver(){
 
 	glEnd();
 }
-
-//------nightRiver---
-void nightRiver(){
+void daySky(){
     //GL_QUADS
     glBegin(GL_QUADS);
-	glColor3ub(21, 67, 96);
+        glColor3ub(84, 153, 199); //up left corner
+    glVertex2f(-1.1f, 1.0f);
+        glColor3ub(127, 179, 213); //up right corner
+	glVertex2f(1.1f, 1.0f);
+        glColor3ub(251, 252, 252); //color mixing
+	glVertex2f(1.1f, -0.1f);
+        glColor3ub(251, 252, 252); //color mixing
+	glVertex2f(-1.1f, -0.4f);
 
-    glVertex2f(-1.0f, -0.4f);
-	glVertex2f(-1.0f, 0.2f);
-	glVertex2f(1.0f, 0.2f);
-    glColor3ub(133, 193, 233); //color mixing
-	glVertex2f(1.0f, -0.15f);
+	glEnd();
+}
+void dayBeach(){
+	//GL_QUADS
+    glBegin(GL_QUADS);
+        glColor3ub(244, 236, 252);
+    glVertex2f(-1.0f, -0.3f); //top left
+        glColor3ub(214, 234, 248);
+    glVertex2f(1.0f, -0.08f); //top right
+        glColor3ub(246, 221, 204);
+	glVertex2f(1.0f, -1.0f); //down right
+        glColor3ub(246, 221, 204);
+	glVertex2f(-1.0f, -1.0f); //down left
 
 	glEnd();
 }
 
-
-void rainyRiver(){
-    //GL_QUADS
+void fornt(){
+	//GL_QUADS
     glBegin(GL_QUADS);
-	glColor3ub(21, 67, 96);
+	glColor3ub(238 ,238, 209);
 
-    glVertex2f(-1.0f, -0.4f);
-	glVertex2f(-1.0f, 0.2f);
-        glColor3ub(153, 163, 164); //color mixing
-	glVertex2f(1.0f, 0.2f);
-	glVertex2f(1.0f, -0.15f);
+	glVertex2f(-1.0f, -1.0f);
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(1.0f, 0.0f);
+        glColor3ub(215, 219, 221);
+	glVertex2f(1.0f, -1.0f);
 
 	glEnd();
+
 }
 
-void eveningRiver(){
-    //GL_QUADS
-    glBegin(GL_QUADS);
-	glColor3ub(191, 201, 202);
 
-    glVertex2f(-1.0f, -0.4f);
-        glColor3ub(93, 109, 126); //color mixing
-	glVertex2f(-1.0f, 0.2f);
-        glColor3ub(133, 146, 158); //color mixing
-	glVertex2f(1.0f, 0.2f);
-        glColor3ub(214, 234, 248); //color mixing
-	glVertex2f(1.0f, -0.15f);
+void init() {
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
 
+void day() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
+
+    //fornt();
+    daySky();
+
+
+    //sun
+    glColor3ub(245, 176, 65);
+	int j;
+	GLfloat x=-0.8f; GLfloat y=0.9f; GLfloat radius =.1f;
+	int triangleAmount = 20; //# of triangles used to draw circle
+
+	//GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+		glVertex2f(x, y); // center of circle
+		for(j = 0; j <= triangleAmount+1;j++) {
+			glVertex2f(
+		            x + (radius * cos(j *  twicePi / triangleAmount)),
+			    y + (radius * sin(j * twicePi / triangleAmount))
+			);
+			glColor3ub(245, 249, 10);
+		}
 	glEnd();
+
+
+
+	glFlush();
+}
+ int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(900,600); // Set the window's initial width & height
+	glutInitWindowPosition(50, 50);
+
+	glutCreateWindow("Day:d , Evening:e ,Night:n ,RainyDay:r ,Start:s ,Stop:a");
+
+    glutDisplayFunc(day);
+    init();
+
+
+
+
+	glutMainLoop();
+	return 0;
+
 }
